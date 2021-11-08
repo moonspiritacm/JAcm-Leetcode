3b5cf2a707ba5c0027a8ffdc4cea8f67266ce3a5
moonspiritacm <yefanbupt@gmail.com>
2021-11-08 17:37:55 +0800

    Problem 0168 ACX

---
+++ src/com/moonspirit/acm/leetcode/p0073/SolutionA.java
@@ -0,0 +1,36 @@
+package com.moonspirit.acm.leetcode.p0073;
+
+class SolutionA {
+    public void setZeroes(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            throw new IllegalArgumentException("非法输入");
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (row[i]) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (col[j]) {
+                for (int i = 0; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
