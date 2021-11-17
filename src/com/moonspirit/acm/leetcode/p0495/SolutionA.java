7f950b2435616e0dc7fea1a44c03d6bad1e578ff
moonspiritacm <yefanbupt@gmail.com>
2021-11-17 17:29:59 +0800

    Problem 0495 ACY

--- src/com/moonspirit/acm/leetcode/p0495/SolutionA.java
+++ src/com/moonspirit/acm/leetcode/p0495/SolutionA.java
@@ -3,12 +3,9 @@ package com.moonspirit.acm.leetcode.p0495;
 /**
  * 模拟。O(n) O(1)
  */
-class Solution {
+class SolutionA {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
-        if (timeSeries == null || timeSeries.length == 0) {
-            throw new IllegalArgumentException("非法输入");
-        }
-        if (duration <= 0) {
+        if (timeSeries == null || timeSeries.length == 0 || duration <= 0) {
             return 0;
         }
 
