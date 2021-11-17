88e6a016354f69c55657663fba180a3766eae47b
moonspiritacm <yefanbupt@gmail.com>
2021-11-17 17:06:25 +0800

    Problem 0168 ACY

--- src/com/moonspirit/acm/leetcode/p0168/SolutionA.java
+++ src/com/moonspirit/acm/leetcode/p0168/SolutionA.java
@@ -15,7 +15,7 @@ class SolutionA {
         StringBuilder sb = new StringBuilder();
         while (columnNumber > 0) {
             columnNumber--;
-            sb.append((char) (columnNumber % 26 + 'A'));
+            sb.append((char) ('A' + columnNumber % 26));
             columnNumber /= 26;
         }
         return sb.reverse().toString();
