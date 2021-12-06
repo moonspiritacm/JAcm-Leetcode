d7f9c4a71fd6cf96af9c59a7cb6227ae77e76ea5
moonspiritacm <yefanbupt@gmail.com>
2021-12-06 15:28:11 +0800

    Problem 0509 ACX

--- src/com/moonspirit/acm/leetcode/p0509/SolutionA.java
+++ src/com/moonspirit/acm/leetcode/p0509/SolutionA.java
@@ -3,8 +3,8 @@ package com.moonspirit.acm.leetcode.p0509;
 /**
  * 动态规划。O(n) O(n)
  * <p>
- * 边界条件：F(0)=0、F(1)=1
- * 状态转移方程：F(n)=F(n-1)+F(n-2)
+ * 边界条件：f(0)=0、f(1)=1
+ * 状态转移方程：f(n)=f(n-1)+f(n-2)
  */
 class SolutionA {
     public int fib(int n) {
