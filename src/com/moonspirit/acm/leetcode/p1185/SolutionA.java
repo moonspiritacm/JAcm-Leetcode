85d513c6d535e44079a374cd2d0cde436befe339
moonspiritacm <yefanbupt@gmail.com>
2022-01-05 17:17:29 +0800

    Problem 1185 ACY

--- src/com/moonspirit/acm/leetcode/p1185/SolutionA.java
+++ src/com/moonspirit/acm/leetcode/p1185/SolutionA.java
@@ -4,17 +4,17 @@ package com.moonspirit.acm.leetcode.p1185;
  * 模拟。O(1) O(1)
  */
 class Solution {
-    static String[] dict = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    static int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static int[] DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static String[] STRS = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
     public String dayOfTheWeek(int day, int month, int year) {
         if (day <= 0 || day > 31 || month <= 0 || month > 12 || year < 1971 || year > 2100) {
             throw new IllegalArgumentException("非法输入");
         }
 
-        // 年份[1970, year - 1]天数，特殊处理闰年
+        // 年份[1971, year - 1]天数，特殊处理闰年
         int sum = 4 + (year - 1971) * 365 % 7;  // 1970-12-31
-        for (int i = 1970; i < year; i++) {
+        for (int i = 1971; i < year; i++) {
             if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
                 sum++;
             }
@@ -22,7 +22,7 @@ class Solution {
 
         // 月份[1, month - 1]天数，特殊处理闰年二月
         for (int i = 1; i < month; i++) {
-            sum += days[i - 1];
+            sum += DAYS[i - 1];
         }
         if (month > 2 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
             sum++;
@@ -30,6 +30,6 @@ class Solution {
 
         // 天数
         sum += day;
-        return dict[sum % 7];
+        return STRS[sum % 7];
     }
 }
