8d91d52159ac5a31aefeeb4a195bf1a6d2791656
moonspiritacm <yefanbupt@gmail.com>
2022-04-13 17:21:24 +0800

    Problem 0796 ACY

--- src/com/moonspirit/acm/leetcode/p0796/SolutionA.java
+++ src/com/moonspirit/acm/leetcode/p0796/SolutionA.java
@@ -4,7 +4,9 @@ package com.moonspirit.acm.leetcode.p0796;
  * 搜索子串。O(n) O(n)
  * <p>
  * s+s包含了所有s通过旋转得到的字符串，只需要检查goal是否为s+s的子串即可。
- * KMP算法复杂度：O(n) O(n)。
+ * <p>
+ * KMP算法时间复杂度 O(n+k)，普通子串匹配算法时间复杂度 O(nk)，其中n和k分别为原串和子串的长度。
+ * Java中的 `str.contains()` 实际调用的是 `indexOf()`，纸面时间复杂度 O(nk)，JVM对其存在优化，导致时间复杂度优于 O(nk)。
  */
 class SolutionA {
     public boolean rotateString(String s, String goal) {
